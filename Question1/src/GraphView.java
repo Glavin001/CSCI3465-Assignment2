4168cb111f72628929c469f317d20c42ef368dbd
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-03-04 19:35:24 -0400

    Closes #1. Added mouse event listeners and saved value back to the model.

--- Question1/src/GraphView.java
+++ Question1/src/GraphView.java
@@ -3,6 +3,10 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Rectangle2D;
 import java.util.Random;
 
@@ -11,13 +15,16 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 
-public class GraphView extends JComponent implements GraphEventObserver 
+public class GraphView extends JComponent implements GraphEventObserver, MouseListener, MouseMotionListener
 {
 	
 	private GraphModel model;
-	
+		
 	public void setModel(GraphModel m) 
 	{
+
+        addMouseListener(this);
+        addMouseMotionListener(this);
 		model = m;
 		model.registerObserver(this);
 		repaint();
@@ -37,25 +44,78 @@ public class GraphView extends JComponent implements GraphEventObserver
 		JFrame frame = (JFrame) SwingUtilities.getRoot(this);
 		Dimension d = frame.getContentPane().getSize();
 		System.out.println(d);
-		
+				
 		int width = this.getWidth()-1;
 		// TODO: Calculate size based off of max value
-		int height = (d.height / 10) * model.getValue();
+		int height = model.getValue();
 		Point offset = new Point(0, d.height-height);
-		
+
+		Rectangle b = this.getBounds();
+		this.setBounds(b.x, b.y, b.width, d.height);
+
 		System.out.println(offset);		
 		System.out.println(width);
 		System.out.println(height);
 		
-		//Rectangle2D r = new Rectangle2D.Double((new Double(width))/10, (new Double(height))/10, (new Double(width))*.8, (new Double(height))*.8);
-		//this.setBounds((int)offset.x,(int)offset.y,(int)width,(int)height);
 		Rectangle2D r = new Rectangle2D.Double(offset.x,offset.y,(int)width,(int)height);
-		//Rectangle2D r = new Rectangle2D.Double((double)x - (double) ((int)x), (double) y - (double) ((int)y), width, height);
-		//g2.setBackground(getBackground());
 		g2.setColor(Color.BLACK);
-		//g2.fill3DRect(r.getX(), r.getY(), width, height, true);
-		//g2.fill(r);
 		g2.draw(r);
+		
+	}
+	
+	private void setValueWithPoint(Point p)
+	{
+		int h = this.getHeight();
+		int v = (int) (h - p.getY());
+		model.setValue(v);
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent event) {
+		// TODO Auto-generated method stub
+		System.out.println("mouseDragged");
+		Point p = event.getPoint();
+		System.out.println(p);
+		setValueWithPoint(p);
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent event) {
+		// TODO Auto-generated method stub
+		System.out.println("mousePressed");
+		Point p = event.getPoint();
+		System.out.println(p);
+		setValueWithPoint(p);
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
